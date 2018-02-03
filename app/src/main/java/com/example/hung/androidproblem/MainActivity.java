9b9500876eea9fb4fa493d8a77581cc4fa076734
hung <hcaelb123@gmail.com>
2018-02-04 02:05:54 +0700

    problem3

---
+++ app/src/main/java/com/example/hung/androidproblem/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.hung.androidproblem;
+
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.btn_sign_up);
+        textView.setPaintFlags(textView.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+    }
+}
